e50ce83e296d2b1828ffdb5ca497c0cdff6bae5e
Saul Vargas <saulvargascosta@hotmail.com>
2024-11-24 01:40:36 -0300

    tabela de precos ok

--- src/app/services/tabela-preco.service.ts
+++ src/app/services/tabela-preco.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { addDoc, collection, doc, Firestore, getDoc, getDocs, updateDoc } from '@angular/fire/firestore';
 import { TabelaPreco } from '../models/tabelaPreco.model';
 
 @Injectable({
@@ -11,8 +11,9 @@ export class TabelaPrecoService {
     private firestore: Firestore,
   ) { }
 
+  /**Adiciona tabela de preço ao Firebase relacionado ao estacionamento */
   async addTabelaPreco(id: string, tabelaPreco: TabelaPreco): Promise<void> {
-console.log('tabelaPreco', tabelaPreco);
+    console.log('tabelaPreco', tabelaPreco);
     const tabelaPrecoRef = collection(this.firestore, `estacionamentos/${id}/tabelaPreco`);
     console.log('tabelaPrecoRef', tabelaPrecoRef);
 
@@ -20,6 +21,38 @@ console.log('tabelaPreco', tabelaPreco);
       await addDoc(tabelaPrecoRef, tabelaPreco);
     } catch (error) {
       console.error('Erro ao adicionar Tabela de Preço:', error);
+    }
+  }
+
+  /**Carrega a Tabela de Preço do estacionamento do Firebase */
+  async getTabelaPreco(id: string): Promise<any> {
+    try {
+      const tabelaPrecoRef = collection(this.firestore, `estacionamentos/${id}/tabelaPreco`);
+      const docSnap = await getDocs(tabelaPrecoRef);
+      const tabelaPreco = docSnap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      return tabelaPreco;
+
+    } catch (error) {
+      console.error('Erro ao buscar Tabela de Preço:', error);
+    }
+  }
+
+  /**Atualiza a Tabela de Preço do estacionamento do Firebase */
+  async updateTabelaPreco(id: string, tabelaPreco: TabelaPreco): Promise<void> {
+    try {
+      const tabelaPrecoRef = doc(this.firestore, `estacionamentos/${id}/tabelaPreco/${tabelaPreco.id}`);
+
+      await updateDoc(tabelaPrecoRef, {
+        preco15Min: tabelaPreco.preco15Min,
+        preco30Min: tabelaPreco.preco30Min,
+        preco1Hora: tabelaPreco.preco1Hora,
+        precoDiaria: tabelaPreco.precoDiaria,
+        precoPernoite: tabelaPreco.precoPernoite,
+        precoMensal: tabelaPreco.precoMensal
+      })
+
+    } catch (error) {
+      console.error('Erro ao atualizar Tabela de Preço:', error);
     }
   }
 }
--- src/app/tabela-preco/tabela-preco.component.ts
+++ src/app/tabela-preco/tabela-preco.component.ts
@@ -18,9 +18,11 @@ export class TabelaPrecoComponent {
     precoDiaria: null,
     precoPernoite: null,
     precoMensal: null
-} as unknown as TabelaPreco;
-  isLoading: boolean = false;
+  } as unknown as TabelaPreco;
   estacionamentoId: string | null = null;
+  tabelaPrecoCarregada: TabelaPreco[] = [];
+  abrirFormularioAtualizacao: boolean = false;
+  abrirFormularioInsersao: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,29 +31,87 @@ export class TabelaPrecoComponent {
 
   ngOnInit() {
     this.estacionamentoId = this.route.snapshot.paramMap.get('id');
+    this.tabelaPRecoService.getTabelaPreco(this.estacionamentoId!)
+      .then(data => {
+        console.log('Dados carregados:', data); // Verifique os dados no console
+        this.tabelaPrecoCarregada = data;
+      })
+      .catch(error => console.error('Erro ao carregar Tabela de Preço:', error));
   }
 
+  /**Metodo para salvar a Tabela de Preço */
   async salvarTabelaPreco(): Promise<void> {
     if (!this.estacionamentoId) {
       console.error('Estacionamento ID não definido!');
       return;
     }
+    if (this.tabelaPrecoCarregada.length > 0) {
+      alert('Tabela de Preço ja cadastrada!');
+      return
+    }
 
-    this.isLoading = true;
     try {
-      console.log('NOVAtabelaPreco', this.novaTabelaPreco);
       await this.tabelaPRecoService.addTabelaPreco(this.estacionamentoId, this.novaTabelaPreco);
       this.tabelaPreco.push(this.novaTabelaPreco);
       console.log('Tabela de Preço adicionada com sucesso!');
-      // Exibir mensagem de sucesso ao usuário
       alert('Tabela de Preço adicionada com sucesso!');
     } catch (error) {
       console.error('Erro ao adicionar Tabela de Preço:', error);
-      // Exibir mensagem de erro ao usuário
       alert('Erro ao adicionar Tabela de Preço. Tente novamente.');
-    } finally {
-      this.isLoading = false;
     }
   }
+
+  /** Carrega os dados da tabela de preços */
+  async carregarTabelaPreco(estacionamentoId: string): Promise<void> {
+    if (!estacionamentoId) {
+      console.error('Estacionamento ID não definido em carregarTabelaPreco!');
+      return;
+    }
+
+    try {
+      const tabelaPrecoResponse = await this.tabelaPRecoService.getTabelaPreco(estacionamentoId);
+
+      if (Array.isArray(tabelaPrecoResponse)) {
+        this.tabelaPrecoCarregada = tabelaPrecoResponse;
+        this.tabelaPreco = [...this.tabelaPrecoCarregada];
+      } else {
+        console.error('Resposta inesperada ao carregar tabela de preços:', tabelaPrecoResponse);
+      }
+
+      console.log('Tabela de Preço carregada com sucesso!', this.tabelaPrecoCarregada);
+    } catch (error) {
+      console.error('Erro ao carregar Tabela de Preço no TS:', error);
+    }
+  }
+
+  /**Função para manipular o formulário de atualização */
+  abrirFormulariosAtualizacao() {
+    this.abrirFormularioAtualizacao = !this.abrirFormularioAtualizacao
+  }
+
+  /**Função para manipular o formulário de insersão */
+  abrirFormulariosInsersao() {
+    this.abrirFormularioInsersao = !this.abrirFormularioInsersao
+  }
+
+  /** Metodo para atualizar a Tabela de Preço */
+  atualizarTabelaPreco(): void {
+    if (!this.tabelaPrecoCarregada || !this.estacionamentoId) {
+      console.error('Dados inválidos: tabela de preço ou ID do estacionamento não definidos!');
+      return;
+    }
+
+    this.tabelaPRecoService
+      .updateTabelaPreco(this.estacionamentoId, this.tabelaPrecoCarregada[0])
+      .then(() => {
+        console.log('Tabela de Preço atualizada com sucesso!');
+        alert('Tabela de Preço atualizada com sucesso!');
+      })
+      .catch(error => {
+        console.error('Erro ao atualizar Tabela de Preço:', error);
+        alert('Erro ao atualizar a tabela de preços. Por favor, tente novamente.');
+      });
+  }
+
 }
 
